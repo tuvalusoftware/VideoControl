5938ab4b2f831cc52188e0cf16a83d2db42bdc70
TouchROI <touchroi@touchrois-mbp.home>
2014-02-15 17:52:25 -0800

    Initial Commit

---
+++ VideoControl/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VideoControl
+//
+//  Created by TouchROI on 2/15/14.
+//  Copyright (c) 2014 TouchROI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
